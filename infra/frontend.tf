8af6269fa4aebab93cf0fbe3411d8dbadaa82e45
colleenrideout <87654872+colleenrideout@users.noreply.github.com>
2024-10-29 10:55:53 -0700

    Add frontend configuration

---
+++ infra/frontend.tf
@@ -0,0 +1,83 @@
+# Frontend Configuration - S3 Bucket & CloudFront
+
+# Create S3 Bucket for Frontend
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket = "frontend-bucket"
+
+  tags = {
+    Name    = "frontend-bucket"
+    Project = var.application_name
+  }
+}
+
+# Specify Private S3 Bucket Access Control Policy
+resource "aws_s3_bucket_acl" "frontend_bucket_acl" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  acl = "private"
+}
+
+# Enable Versioning on the S3 Bucket
+resource "aws_s3_bucket_versioning" "frontend_bucket_versioning" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Create OAI for Frontend Bucket
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "OAI for secure S3 access"
+}
+
+# Create Policy to Allow OAI Access Frontend S3 Bucket
+resource "aws_s3_bucket_policy" "frontend_policy" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "AWS": aws_cloudfront_origin_access_identity.oai.iam_arn  # Allow OAI-verified Access
+        },
+        "Action": "s3:GetObject",
+        "Resource": "${aws_s3_bucket.frontend_bucket.arn}/*" # Access is to frontend bucket resource
+      }
+    ]
+  })
+}
+
+# Create CloudFront Web Distribution to Route User Request
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    domain_name = aws_s3_bucket.frontend_bucket.bucket_regional_domain_name
+    origin_id   = "S3-Frontend"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "S3-Frontend"
+    forwarded_values {
+      query_string = false
+    }
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = {
+    Name    = "frontend-distribution"
+    Project = var.application_name
+  }
+}
