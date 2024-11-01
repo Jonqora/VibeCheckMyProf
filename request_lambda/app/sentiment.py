3a84c298de41c1ab9f7ebdb6f7f3e6be8d3cd2c9
Ellen Lloyd <erlloyd@student.ubc.ca>
2024-11-01 12:54:03 -0700

    Add timing points

--- request_lambda/app/sentiment.py
+++ request_lambda/app/sentiment.py
@@ -13,9 +13,11 @@ import torch
 from textblob import TextBlob
 import boto3
 from spellchecker import SpellChecker
+import time
 
 class SentimentAnalyzer:
     def __init__(self):
+        start_time = time.perf_counter()
         # Load the tokenizer and model from the local directory
         self.tokenizer = AutoTokenizer.from_pretrained("./models/goemotions-tokenizer")
         self.model = AutoModelForSequenceClassification.from_pretrained("./models/goemotions-model")
@@ -23,6 +25,8 @@ class SentimentAnalyzer:
         self.emotion_labels = self.model.config.id2label
         self.comprehend = boto3.client('comprehend', region_name="ca-central-1")
         self.spellchecker = SpellChecker()
+        end_time = time.perf_counter()
+        print(f"Time to init SentimentAnalyzer: {(end_time - start_time):.4f} seconds")
 
     def analyze_sentiment_textblob(self, text):
         blob = TextBlob(text)
@@ -56,13 +60,26 @@ class SentimentAnalyzer:
 def analyze(professor_json: Dict[str, Any]) -> Dict[str, Any]:
     sentiment_analyzer = SentimentAnalyzer()
 
+    TextBlob_time = 0
+    GoEmotions_time = 0
+    Comprehend_time = 0
+    Spelling_time = 0
+
     for review in professor_json["reviews"]:
         comment = review["comment"]
-
+        time1 = time.perf_counter()
         tb_polarity, tb_subjectivity = sentiment_analyzer.analyze_sentiment_textblob(comment)
+        time2 = time.perf_counter()
+        TextBlob_time += time2 - time1
         emotion = sentiment_analyzer.analyze_emotion_goemotions(comment)
+        time3 = time.perf_counter()
+        GoEmotions_time += time3 - time2
         comprehend_sentiment = sentiment_analyzer.analyze_sentiment_comprehend(comment)
+        time4 = time.perf_counter()
+        Comprehend_time += time4 - time3
         spelling_quality, spelling_errors = sentiment_analyzer.analyze_spelling_and_grammar(comment)
+        time5 = time.perf_counter()
+        Spelling_time += time5 - time4
 
         review["vcmp_polarity"] = tb_polarity
         review["vcmp_subjectivity"] = tb_subjectivity
@@ -70,5 +87,10 @@ def analyze(professor_json: Dict[str, Any]) -> Dict[str, Any]:
         review["vcmp_sentiment"] = comprehend_sentiment
         review["vcmp_spellingerrors"] = spelling_errors
         review["vcmp_spellingquality"] = spelling_quality
+
+    print(f"Time for TextBlob analysis: {TextBlob_time:.4f} seconds.")
+    print(f"Time for GoEmotions analysis: {GoEmotions_time:.4f} seconds.")
+    print(f"Time for Comprehend analysis: {Comprehend_time:.4f} seconds.")
+    print(f"Time for Spelling analysis: {Spelling_time:.4f} seconds.")
 
     return professor_json
