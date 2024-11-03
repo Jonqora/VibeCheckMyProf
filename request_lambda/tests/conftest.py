2dee85319eebcd1434deea3ae42cb2e9b4dce307
colleenrideout <87654872+colleenrideout@users.noreply.github.com>
2024-11-03 13:26:32 -0800

    Add database tests

---
+++ request_lambda/tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from request_lambda.app.config import Config, TSuiteConfig
+
+
+@pytest.fixture(scope="session")
+def session_monkeypatch():
+    """ Creates a pytest-session-scoped monkeypatch fixture. """
+    from _pytest.monkeypatch import MonkeyPatch
+    mp = MonkeyPatch()
+    yield mp
+    mp.undo()
+
+
+@pytest.fixture(autouse=True, scope="session")
+def replace_config(session_monkeypatch):
+    """ Replaces configuration with test db name to
+        prevent tests from altering production db. """
+    test_config_path = "infra/config.env"
+
+    def mock_config(cls):
+        return TSuiteConfig.from_file(test_config_path)
+
+    session_monkeypatch.setattr(Config, "from_env", mock_config)
