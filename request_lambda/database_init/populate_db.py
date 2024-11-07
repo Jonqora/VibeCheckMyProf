ee5f4978b574d14e95bd2b88c5702fbedcf0c0bc
Ellen Lloyd <erlloyd@student.ubc.ca>
2024-11-07 14:53:47 -0800

    Fix duplicate function

--- request_lambda/database_init/populate_db.py
+++ request_lambda/database_init/populate_db.py
@@ -30,25 +30,6 @@ def process_json_files_in_folder(folder_name):
             print(f"Inserted data from {filename}")
 
 
-
-def process_json_files_in_folder(folder_name):
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    folder_path = os.path.join(script_dir, folder_name)
-
-    # Ensure the folder exists
-    if not os.path.exists(folder_path):
-        print(f"The folder '{folder_name}' does not exist.")
-        return
-
-    # Iterate over all files in the specified folder
-    for filename in os.listdir(folder_path):
-        file_path = os.path.join(folder_path, filename)
-        # Check if the file is a JSON file
-        if filename.endswith('.json'):
-            database.insert_data_from_json_file(file_path, config)
-            print(f"Inserted data from {filename}")
-
-
 print("Removing old tables from database...")
 config = Config().from_file(env_file_path)
 database.run_sql_file(drop_tables_file_path, config)
